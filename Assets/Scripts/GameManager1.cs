c3ecd1e2b84a3a92fa9a9b59e3306903e806b559
JacTimDav <33136649+JacTimDav@users.noreply.github.com>
2019-10-16 19:50:18 -0700

    Add Commit Push

--- Assets/Scripts/GameManager1.cs
+++ Assets/Scripts/GameManager1.cs
@@ -7,19 +7,19 @@ public class GameManager1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Update()
     }
 
     // Update is called once per frame
     void Update()
     {
-        //If(InputManager.Restart == true)
+        //If(InputManager.Restart == true) //This means "Reset" is pressed
         //      make a copy of the GameObject to save
         //      reset the original scene
         //      reset the original GameObject
         //      load the copied GameObject data to the original GameObject
-                //(Load score, game progress, etc.)
-                //Pins connected = 0
-                //Game over state = false
+        //(Load score, game progress, etc.)
+        //Pins connected = 0
+        //Game over state = false
+        print("It works!");
     }
 }
