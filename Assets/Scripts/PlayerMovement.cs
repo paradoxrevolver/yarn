14ddbe37afabdc1b172594db2aec3dea8b0c4085
WilliamBrettSJSU <william.brett@sjsu.edu>
2019-10-26 13:50:30 -0700

    MovementBasic
    Implimented basic wasd movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0f, 0f, 1f * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-1f * Time.deltaTime, 0f, 0f);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0f, 0f, -1f * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(1f * Time.deltaTime, 0f, 0f);
+        }
     }
 }
