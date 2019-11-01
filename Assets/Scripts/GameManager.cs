e43f0a34be5dd6e5ac5199077e865116cebb6b79
rubic <adamtheball@gmail.com>
2019-11-01 16:04:39 -0700

    small changes
    removed some unnecessary code. adjusted the scaling of the Nail and gave it a collider.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,15 +6,15 @@ using UnityEngine.SceneManagement;
  */
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
+    private static GameManager _instance;
     
     private void Awake()
     {
         // Ensures that the GameObject with this script is a Singleton:
         // a duplicate of this script will always be deleted
         // and this GameObject will not be destroyed on scene change.
-        instance = this;
-        if (instance != null && instance != this)
+        _instance = this;
+        if (_instance != null && _instance != this)
         {
             Destroy(this.gameObject);
             return;
--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour {
-    public void OnMove(InputAction.CallbackContext ctx) {
-        Vector2 movement = ctx.ReadValue<Vector2>();
-        Debug.Log(movement);
-    }
+    
 }
