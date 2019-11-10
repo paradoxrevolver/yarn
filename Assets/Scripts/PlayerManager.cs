887c3bd0f563a8c5b53c0c7b7bffa168c6fefeb9
JacTimDav <33136649+JacTimDav@users.noreply.github.com>
2019-11-09 16:13:37 -0800

    ok

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public enum State
+    {
+        Normal,
+        Holding,
+        Pulling
+    }
+    private State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        state = State.Normal;
+    }
+    public void SetState(State state)
+    {
+        this.state = state;
+    }
+}
