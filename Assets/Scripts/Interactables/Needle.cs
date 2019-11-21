d02aabed5decf21e093ff1c073cfa17a03d8e931
rubic <adamtheball@gmail.com>
2019-11-20 18:01:42 -0800

    more work on the Needle

--- Assets/Scripts/Interactables/Needle.cs
+++ Assets/Scripts/Interactables/Needle.cs
@@ -1,5 +1,22 @@
+using UnityEngine.UI;
+
 public class Needle : Interactable {
+    public enum State {
+        Raised,
+        Lowered,
+    }
+
+    private State state;
+
     public override void Interact() {
+        ToggleState();
+        UpdateMesh();
+    }
+
+    private void UpdateMesh() { 
         
     }
+
+    private void ToggleState() { state = state == State.Raised ? State.Lowered : State.Raised; }
+
 }
