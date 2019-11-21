f18bf04819754ac3bb7330323741fe3848af09ad
rubic <adamtheball@gmail.com>
2019-11-20 21:26:32 -0800

    getting yarn started, need to make the object possible to pick up

--- Assets/Scripts/Interactables/Yarn.cs
+++ Assets/Scripts/Interactables/Yarn.cs
@@ -1,5 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class Yarn : Interactable {
+    private struct Contact {
+        private Vector3 position;
+        private float interiorAngle;
+        private bool isClockwise;
+        private GameObject source;
+    }
+    private List<Contact> contacts;
+    
+    public enum State {
+        Normal,
+        Held,
+        Unraveling,
+        Settled,
+        Destroyed,
+    }
+    private State state;
+
+    public bool IsDestroyed() { return state == State.Destroyed; }
+
+    protected override void Awake() {
+        base.Awake();
+        state = State.Normal;
+    }
+
+    private void FixedUpdate() {
+        RaycastHit hit;
+        
+    }
+
     public override void Interact() {
         print("Yarn was interacted with and nothing happened.");
+    }
+
+    private void OnDrawGizmos() {
+        
     }
 }
