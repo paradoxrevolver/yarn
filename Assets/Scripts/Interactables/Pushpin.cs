2fcbabffdff32b86d76d7f9862eb555b08fe7176
rubic <adamtheball@gmail.com>
2019-11-28 17:53:21 -0800

    yarn can now be attached and detached from any pushpin and link properly, removing and returning control of the yarn line to the player as appropriate.

--- Assets/Scripts/Interactables/Pushpin.cs
+++ Assets/Scripts/Interactables/Pushpin.cs
@@ -4,33 +4,82 @@ public class Pushpin : Interactable {
         Tied,
         Done,
     }
-
     private State state;
+    
+    public bool IsDone() { return state == State.Done; }
 
+    // the yarn that is connected to this pushpin. can only be one
+    private Yarn connectedYarn;
+    
     protected override void Awake() {
         base.Awake();
         state = State.Untied;
+        connectedYarn = null;
     }
-
-    public void SetState(State newState) { state = newState; }
-    public bool IsDone() { return state == State.Done; }
 
     public override void Interact() {
-        if(playerManager.CheckState(PlayerManager.State.Holding)
-                && state == State.Untied) 
-            AttachYarn(playerManager.YarnHeld);
+        YarnUpdate();
     }
 
-    private void AttachYarn(Yarn yarn) {
-        state = State.Tied;
-        if(playerManager.CheckState(PlayerManager.State.Holding))
-            playerManager.SetState(PlayerManager.State.Pulling);
-        yarn.AddContact(new Yarn.Contact(gameObject));
-        print("The player just attached yarn to a pushpin.");
-    }
+    /**
+     * A Pushpin will attempt to update the player and any yarn they're holding
+     * based on the states of those objects.
+     */
+    private void YarnUpdate() {
+        Yarn playerYarn = playerManager.YarnHeld;
+        
+        /*
+         * Yarn can be attached to this Pushpin if:
+         * - This Pushpin is Untied to anything
+         *     - The player is holding onto yarn, in which case the player wants to start a line of yarn
+         *     - The player is pulling yarn, in which case the player wants to finish a line of yarn
+         */
+        if (state == State.Untied) {
+            if (playerManager.CheckState(PlayerManager.State.Holding)) {
+                // start a line of yarn
+                state = State.Tied;
+                playerManager.SetState(PlayerManager.State.Pulling);
+                connectedYarn = playerYarn;
+                connectedYarn.AddContact(gameObject);
+                print("Yarn was just tied to a Pushpin, starting a line.");
 
-    private void DetachYarn() {
-        state = State.Untied;
-        playerManager.SetState(PlayerManager.State.Holding);
+            } else if (playerManager.CheckState(PlayerManager.State.Pulling)) {
+                // finish a line of yarn
+                state = State.Tied;
+                connectedYarn = playerYarn;
+                connectedYarn.TieOff(this);
+                print("Yarn was just tied to a Pushpin, ending a line.");
+            }
+        }
+        
+        /*
+         * Yarn can be detached from this Pushpin if:
+         * - This Pushpin is Tied OR Done already
+         *     - The player is pulling yarn, which case they want to return to just holding yarn
+         *         - This Pushpin must be part of this line of yarn AND be the only thing other than the player
+         *     - The player is normal, in which case they want to change an existing line of yarn
+         *         - This Pushpin must be either the first or last in the line of yarn AND there are at least two contacts
+         */
+        else if (state == State.Tied || state == State.Done) {
+            if (playerManager.CheckState(PlayerManager.State.Pulling)
+                    && connectedYarn.ContactCount() == 2
+                    && connectedYarn == playerYarn) {
+                // return to holding yarn
+                state = State.Untied;
+                playerManager.SetState(PlayerManager.State.Holding);
+                connectedYarn.RemoveContactAll(gameObject);
+                connectedYarn = null;
+                print("Yarn was just untied from a Pushpin, undoing a line.");
+                
+            } else if (playerManager.CheckState(PlayerManager.State.Normal)
+                    && connectedYarn.ContactCount() >= 2
+                    && connectedYarn.IsContactAtEnd(gameObject)) {
+                // start changing a line of yarn again
+                state = State.Untied;
+                connectedYarn.UntieFrom(this);
+                connectedYarn = null;
+                print("Yarn was just untied from a Pushpin, to edit a line.");
+            }
+        }
     }
 }
--- Assets/Scripts/Interactables/Yarn.cs
+++ Assets/Scripts/Interactables/Yarn.cs
@@ -1,31 +1,9 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Yarn : Interactable {
     public Vector3 positionYarnInPlayersArms;
-    
-    /**
-     * A Contact represents a single node in the list of objects that
-     * this Yarn object is connected to.
-     */
-    public struct Contact {
-        // the GameObject that owns this Contact
-        public GameObject source;
-        
-        // the angle from the previous Contact to the next
-        // note that this value is null for the last Contact
-        public float angle;
-        
-        // a list of calculated points that the yarn is actually rendered at
-        public List<Vector3> renderPoints;
-
-        public Contact(GameObject source, float angle = 0) {
-            this.source = source;
-            this.angle = angle;
-            renderPoints = new List<Vector3>();
-        }
-    }
-    private List<Contact> contacts;
 
     public enum State {
         Normal,
@@ -34,6 +12,83 @@ public class Yarn : Interactable {
     private State state;
 
     public bool IsDestroyed() { return state == State.Destroyed; }
+
+    /**
+     * PotentialContacts are all objects which could potentially become
+     * a Contact. Contacts maintain a list of these and raises an alarm
+     * if they 
+     */
+    public struct PotentialContact {
+        // the GameObject that could become a potential Contact
+        public GameObject source;
+        
+        // the angle this PotentialContact has with the owning Contact
+        public float parentAngle;
+    }
+    
+    /**
+     * A Contact represents a single node in the list of objects that
+     * this Yarn object is connected to.
+     */
+    public class Contact {
+        // the GameObject that owns this Contact
+        public GameObject source;
+        
+        // the angle from this Contact to the next Contact
+        // calculated on the XZ plane relative to Vector3.right
+        // note that this value is null for the last contact in a list
+        public float angle;
+        
+        // a list of calculated points that the yarn is actually rendered at
+        public List<Vector3> renderPoints;
+
+        // a list of potential contacts from the previous frame, for anticipating new contacts
+        public List<PotentialContact> oldPotential;
+
+        public Contact(GameObject source, float angle = 0) {
+            this.source = source;
+            this.angle = angle;
+            renderPoints = new List<Vector3>();
+            oldPotential = new List<PotentialContact>();
+        }
+    }
+    private List<Contact> contacts;
+
+    public void AddContact(GameObject obj) {
+        contacts.Add(new Contact(obj));
+    }
+    
+    /* Remove all Contacts with obj as its source. */
+    public void RemoveContactAll(GameObject obj) {
+        foreach(Contact c in contacts.FindAll(c => c.source == obj))
+            contacts.Remove(c);
+    }
+    
+    /*
+     * Remove a Contacts with obj as its source only at the beginning or end of the contacts list.
+     *
+     * Additionally causes the contacts list to reverse order if trying to remove the contact from the front.
+     * This is done in the event that the player tries to remove the yarn from the opposite end, so that
+     * the yarn's contacts are always added from the end of the list.
+     */
+    public void RemoveContactFromEnd(GameObject obj) {
+        if(contacts[0].source == obj) contacts.Reverse();
+        if(contacts[contacts.Count - 1].source == obj) contacts.RemoveAt(contacts.Count - 1);
+    }
+
+    /* Does the contacts list contain any Contact with obj as a source? */
+    public bool ContainsContact(GameObject obj) {
+        return contacts.Exists(c => c.source == obj);
+    }
+
+    /* Is obj a Contact at the beginning or end of the contacts list? */
+    public bool IsContactAtEnd(GameObject obj) {
+        return contacts[0].source == obj || contacts[contacts.Count - 1].source == obj;
+    }
+    
+    public int ContactCount() { return contacts.Count; }
+
+    public GameObject mesh;
 
     protected override void Awake() {
         base.Awake();
@@ -67,14 +122,15 @@ public class Yarn : Interactable {
         // the player picks up the yarn if they have their arms free
         if (playerManager.CheckState(PlayerManager.State.Normal)) {
             PickUp();
-            AddContact(new Contact(player));
         }
     }
 
-    public void AddContact(Contact c) { contacts.Add(c); }
-    public void RemoveContact(Contact c) { contacts.Remove(c); }
+    private void HideMesh() { mesh.SetActive(false); }
+
+    private void ShowMesh() { mesh.SetActive(true); }
 
     private void PickUp() {
+        AddContact(player.gameObject);
         playerManager.SetState(PlayerManager.State.Holding);
         playerManager.YarnHeld = this;
         transform.SetParent(player.transform);
@@ -88,6 +144,22 @@ public class Yarn : Interactable {
         transform.parent = null;
         transform.localPosition = player.transform.localPosition;
         print("The player has dropped the yarn.");
+    }
+
+    /* Untie this Yarn object from the given Pushpin, returning control to the player. */
+    public void UntieFrom(Pushpin pushpin) {
+        playerManager.SetState(PlayerManager.State.Pulling);
+        RemoveContactFromEnd(pushpin.gameObject);
+        AddContact(player.gameObject);
+        if(mesh) ShowMesh();
+    }
+    
+    /* Ties this Yarn off on the given Pushpin, removing control from the player. */
+    public void TieOff(Pushpin pushpin) {
+        playerManager.SetState(PlayerManager.State.Normal);
+        RemoveContactFromEnd(player);
+        AddContact(pushpin.gameObject);
+        if(mesh) HideMesh();
     }
 
     private void OnDrawGizmos() {
