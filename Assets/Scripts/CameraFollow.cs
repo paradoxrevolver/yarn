7a9ca9c92b5431bbeeec41fbd6005c1828cae8e1
WilliamBrettSJSU <william.brett@sjsu.edu>
2020-01-01 15:30:18 -0800

    CameraUpdate
    Camera now follows the player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    
+    private Vector3 initialpos;
+    private Vector3 updatepos;
+    private Vector3 adjustpos;
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+
+        initialpos = player.transform.position;
+    }
+    private void LateUpdate()
+    {
+        updatepos = player.transform.position;
+        updatepos = initialpos + updatepos;
+        updatepos.z = updatepos.z - 20;
+        updatepos.y = initialpos.y+20;
+        transform.position = updatepos;
+    }
 }
