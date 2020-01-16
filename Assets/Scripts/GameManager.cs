a6dba984c01f247301eea3d73da94fa1c71f1734
JacTimDav <33136649+JacTimDav@users.noreply.github.com>
2020-01-15 17:33:18 -0800

    Adding Levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,11 +17,14 @@ public class GameManager : MonoBehaviour {
         // Ensures that the GameObject with this script is a Singleton:
         // a duplicate of this script will always be deleted
         // and this GameObject will not be destroyed on scene change.
-        instance = this;
         if (instance != null && instance != this)
         {
             Destroy(this.gameObject);
             return;
+        }
+        else
+        {
+            instance = this;
         }
         DontDestroyOnLoad(this.gameObject);
         
