9508aa4c84c1bb222565fb2b479bdd0124da63a1
JacTimDav <33136649+JacTimDav@users.noreply.github.com>
2020-02-05 15:42:00 -0800

    Numbers on doors

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,8 +1,13 @@
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class Door : Interactable {
     public string goToLevel;
+    public Text doorText;
     public override void Interact() {
         SceneManager.LoadScene(goToLevel);
+    }
+    private void Awake()
+    {
+        doorText.text = goToLevel;
     }
 }
