2d72a93cdbc3dcf4946714cc73495d67d0e50fc6
JacTimDav <33136649+JacTimDav@users.noreply.github.com>
2020-02-19 16:18:02 -0800

    Return of the door number

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,9 +1,12 @@
+
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class Door : Interactable {
+public class Door : Interactable
+{
     public string goToLevel;
     public Text doorText;
-    public override void Interact() {
+    public override void Interact()
+    {
         SceneManager.LoadScene(goToLevel);
     }
     private void Awake()
