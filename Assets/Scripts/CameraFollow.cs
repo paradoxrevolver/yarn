a4732c25b5c72f3276e65ae0730d40f2e6f887e6
rubic <adamtheball@gmail.com>
2020-05-02 14:44:35 -0700

    fixed graphical glitch
    did this by constraining the values that the main camera can be set to

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,16 +9,19 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 0.125f;
     
 
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         offset = new Vector3(0, 11, -9);
         Vector3 desiredPos = player.position + offset;
 
         // If smooth scroll wanted, use this line of code, although player looks laggy
         Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
-        transform.position = smoothedPos;
+        transform.position = RoundVector(smoothedPos, 0.01f);
+    }
 
-
-
+    Vector3 RoundVector(Vector3 v, float roundTo) {
+        return new Vector3(
+            Mathf.Round(v.x / roundTo) * roundTo, 
+            Mathf.Round(v.y / roundTo) * roundTo, 
+            Mathf.Round(v.z / roundTo) * roundTo);
     }
 }
